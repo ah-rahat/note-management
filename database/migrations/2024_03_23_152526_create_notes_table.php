b0684d453c50a9d6faada154edab16e41c454105
unknown <ahrahat15398@gmail.com>
2024-03-24 22:04:10 +0600

    -

--- database/migrations/2024_03_23_152526_create_notes_table.php
+++ database/migrations/2024_03_23_152526_create_notes_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
              $table->id();
             $table->string('title');
             $table->text('content');
-         
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
